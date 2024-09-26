f49b2d28febccc7eb353d3cfeebd4e89d14b188e
ETATUNG\Klay.Huang <klay.huang@etatung.com>
2024-09-26 17:55:56 +0800

    Initial commit at 20240926

---
+++ assignment1_ConsoleJob/assignment1_ConsoleJob/Program.cs
@@ -0,0 +1,81 @@
+﻿using Microsoft.Extensions.Configuration;
+using Serilog;
+
+namespace assignment1_ConsoleJob
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                var config = new ConfigurationBuilder()
+                    .AddYamlFile("SerilogConfig.yml")
+                    .AddYamlFile("SysConfig.yml")
+                    .Build();
+
+                Log.Logger = new LoggerConfiguration()
+                .ReadFrom.Configuration(config)
+                .CreateLogger();
+
+                string sourceDirectory = config["SourceDirectory"]!;
+                string destinationDirectory = config["DestinationDirectory"]!;
+                Log.Information($"來源資料夾: {sourceDirectory}, 目的資料夾: {destinationDirectory}");
+
+                if (!Directory.Exists(sourceDirectory))
+                {
+                    Log.Error($"來源資料夾不存在!!");
+                    return;
+                }
+
+                if (!Directory.Exists(destinationDirectory))
+                {
+                    Directory.CreateDirectory(destinationDirectory);
+                }
+
+                MoveFiles(sourceDirectory, destinationDirectory);
+            }
+            catch (Exception ex)
+            {
+                Log.Error(ex, "移動檔案時發生錯誤");
+            }
+            finally
+            {
+                Log.CloseAndFlush();
+            }
+        }
+
+        static void MoveFiles(string sourceDirectory, string destinationDirectory)
+        {
+            foreach (var file in Directory.GetFiles(sourceDirectory))
+            {
+                string fileName = Path.GetFileName(file);
+                string destFile = Path.Combine(destinationDirectory, fileName);
+
+                File.Copy(file, destFile, true);
+                Log.Information($"{fileName} 複製到 {destinationDirectory} ");
+
+                File.Delete(file);
+                Log.Information($"來源檔案刪除: {fileName}");
+            }
+
+            foreach (var subDirectory in Directory.GetDirectories(sourceDirectory))
+            {
+                string subDirectoryName = Path.GetFileName(subDirectory);
+                string destSubDirectory = Path.Combine(destinationDirectory, subDirectoryName);
+
+                if (!Directory.Exists(destSubDirectory))
+                {
+                    Directory.CreateDirectory(destSubDirectory);
+                }
+
+                MoveFiles(subDirectory, destSubDirectory);
+
+                Directory.Delete(subDirectory, true);
+                Log.Information($"資料夾刪除: {subDirectory}");
+            }
+        }
+
+
+    }
+}
